be0a4a39e27518d9c9c11554c3c8f5486cc2672e
Zilla Chen <zillachan@gmail.com>
2017-09-26 11:25:13 +0800

    init commit
    init commit

---
+++ producer/src/main/java/pub/zilla/mq/Producer.java
@@ -0,0 +1,63 @@
+package pub.zilla.mq;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    private static final String EXCHANGE_NAME = "topic_logs";
+
+    public static void main(String[] argv) throws IOException, TimeoutException {
+        Connection connection = null;
+        Channel channel = null;
+        try {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+
+            String routingKey = getRouting(argv);
+            String message = getMessage(argv);
+
+            channel.basicPublish(EXCHANGE_NAME, routingKey, null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + routingKey + "':'" + message + "'");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception ignore) {
+                }
+            }
+        }
+    }
+
+    private static String getRouting(String[] strings) {
+        if (strings.length < 1)
+            return "anonymous.info";
+        return strings[0];
+    }
+
+    private static String getMessage(String[] strings) {
+        if (strings.length < 2)
+            return "Hello World!";
+        return joinStrings(strings, " ", 1);
+    }
+
+    private static String joinStrings(String[] strings, String delimiter, int startIndex) {
+        int length = strings.length;
+        if (length == 0) return "";
+        if (length < startIndex) return "";
+        StringBuilder words = new StringBuilder(strings[startIndex]);
+        for (int i = startIndex + 1; i < length; i++) {
+            words.append(delimiter).append(strings[i]);
+        }
+        return words.toString();
+    }
+}
